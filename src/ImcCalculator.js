9973e6fd82a0fe5a1c67b968ecedf789ce7e286f
zomervinicius <vinicius.zomer123@gmail.com>
2019-09-15 16:07:08 -0300

    working example

--- src/ImcCalculator.js
+++ src/ImcCalculator.js
@@ -1,19 +1,44 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Slider from "./Slider"
 
 export default function ImcCalculator() {
+  const [weight, setWeight] = useState(60)
+  const [height, setHeight] = useState(1.6)
+  const [imcValue, setImcValue] = useState(0)
+  const onWeightChange = e => {
+    setWeight(e.target.value)
+  }
+  const onHeightChange = e => {
+    setHeight(e.target.value)
+  }
+  useEffect(() => {
+    const calcImc = () => {
+      setImcValue((weight / (height * height)).toFixed(1))
+    }
+    calcImc()
+  }, [height, weight])
   return (
     <div>
-      <Slider min={40} max={180} value={60} label={"Peso"} suffix={"kg"} />
+      <Slider
+        min={40}
+        max={180}
+        value={weight}
+        label={"Peso"}
+        step={1}
+        suffix={"kg"}
+        onChange={onWeightChange}
+      />
       <Slider
         min={1.5}
         max={2.0}
-        value={1.6}
+        step={0.01}
+        value={height}
         label={"Altura"}
         suffix={"m"}
+        onChange={onHeightChange}
         isDecimal
       />
-      <span>IMC: </span>
+      <span>IMC: {imcValue}</span>
     </div>
   )
 }
--- src/Slider.js
+++ src/Slider.js
@@ -1,15 +1,32 @@
 import React from "react"
 
-export default function Slider({ min, max, value, label, suffix, isDecimal }) {
-  const replaceDot =
-    value
-      .toString()
-      .replace(".", ",")
-      .replace(" ", "") + 0
+export default function Slider({
+  min,
+  max,
+  value,
+  label,
+  suffix,
+  isDecimal,
+  onChange,
+  step
+}) {
+  const replaceDot = parseFloat(value)
+    .toFixed(2)
+    .toString()
+    .replace(".", ",")
+    .replace(" ", "")
+
   return (
     <div>
       <span>{`${label}: ${isDecimal ? replaceDot : value}${suffix} `}</span>
-      <input type="range" min={min} max={max} value={value}></input>
+      <input
+        type="range"
+        min={min}
+        max={max}
+        value={value}
+        onChange={onChange}
+        step={step}
+      ></input>
     </div>
   )
 }
